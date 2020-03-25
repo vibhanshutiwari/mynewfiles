6e74ce1269f8c56551f1a8681d62c6f432fcfff2
Test <odbc@infra.cyntaxia.com>
2020-03-25 19:01:04 +0000

    newchange

--- practice.js
+++ practice.js
@@ -1,15 +1,5 @@
 
-const obj = [
-  {
-    name:'shubham',
-    salary:10000,
-  },
-  {
-    name:'aman',
-    salary:15000,
-  }
-];
-
-const newobj = obj => obj.salary < 20000;
-const result = obj.every(newobj);
-console.log(result);
+const newhello = (name,age) => {
+  console.log(name);
+}
+ newhello('shubham');
